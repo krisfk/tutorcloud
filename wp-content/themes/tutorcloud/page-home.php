ae7e4ad9dc404176e4371860fa4c22c0c7e23f7e
kaiyu li <krisfk@gmail.com>
2021-11-17 17:02:32 +0800

    .

--- wp-content/themes/tutorcloud/page-home.php
+++ wp-content/themes/tutorcloud/page-home.php
@@ -14,13 +14,15 @@ get_header();
 <div class="home-banner-div">
 
 
-    <div class="banner-msg pt-5">
-        開業優惠！只收導師 <span class="large-txt">1.5星期</span> 學費作行政費 <br>
-        家長可獲高達 <span class="large-txt">$100</span> 現金回贈
-    </div>
+    <div class="home-banner-content">
+        <div class="banner-msg">
+            開業優惠！只收導師 <span class="large-txt">1.5星期</span> 學費作行政費 <br>
+            家長可獲高達 <span class="large-txt">$100</span> 現金回贈
+        </div>
 
-    <div class="text-center mt-5">
-        <a href="#" class="know-more-btn">了解更多</a>
+        <div class="text-center mt-5">
+            <a href="#" class="know-more-btn">了解更多</a>
+        </div>
     </div>
 </div>
 <?php
