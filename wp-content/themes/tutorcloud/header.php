6476c324d4261b7d6977e7006a733ab21128946f
kaiyu li <krisfk@gmail.com>
2021-11-17 16:28:19 +0800

    .

--- wp-content/themes/tutorcloud/header.php
+++ wp-content/themes/tutorcloud/header.php
@@ -46,7 +46,7 @@
 
                             <div class="row">
                                 <div class="col-4">
-
+                                    <img src="<?php echo get_template_directory_uri();?>/assets/images/logo.png" alt="">
                                 </div>
                                 <div class="col-6"></div>
                                 <div class="col-2"></div>
