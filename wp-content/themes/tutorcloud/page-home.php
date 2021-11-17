a734ba8c8820dc628f7fa8a600653fee1e790a35
kaiyu li <krisfk@gmail.com>
2021-11-17 17:23:41 +0800

    .

--- wp-content/themes/tutorcloud/page-home.php
+++ wp-content/themes/tutorcloud/page-home.php
@@ -37,11 +37,19 @@ get_header();
     <div class="row gx-5 ">
         <div class="col-4"> <img class="w-100" src="<?php echo get_template_directory_uri();?>/assets/images/icon-1.png"
                 alt="">
+            <div class="pt-title">優良師資</div>
+            <div>所有導師均為現職大學生及具有學士學位或以上資歷，確保導師知識水平及能力，為學生提供最優質的教育。</div>
         </div>
         <div class="col-4"><img class="w-100" src="<?php echo get_template_directory_uri();?>/assets/images/icon-2.png"
-                alt=""></div>
+                alt="">
+            <div class="pt-title">簡易登記</div>
+            <div>登記過程簡易快速，只需填寫個案基本資料，最快2分鐘內完成登記。</div>
+        </div>
         <div class="col-4"><img class="w-100" src="<?php echo get_template_directory_uri();?>/assets/images/icon-3.png"
-                alt=""></div>
+                alt="">
+            <div class="pt-title">彈性課堂</div>
+            <div>學生可以自由選擇上課地點及時間，不論是上門、會所、視像補習都由學生自行決定。不像傳統補習社限制時間和地點，私人補習配對解決空間和時間的限制。</div>
+        </div>
     </div>
 </div>
 <?php
