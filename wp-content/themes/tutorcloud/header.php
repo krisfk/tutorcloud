7f2661642b19d0acc514126c935aed2a40ecb8d1
kaiyu li <krisfk@gmail.com>
2021-11-17 15:52:30 +0800

    .

--- wp-content/themes/tutorcloud/header.php
+++ wp-content/themes/tutorcloud/header.php
@@ -14,19 +14,124 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?> <?php twentytwentyone_the_html_classes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<?php wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <?php wp_head(); ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'twentytwentyone' ); ?></a>
+    <?php wp_body_open(); ?>
+    <div id="page" class="site">
+        <a class="skip-link screen-reader-text" href="#content">
+            <?php esc_html_e( 'Skip to content', 'tutorcloud' ); ?></a>
 
-	<?php get_template_part( 'template-parts/header/site-header' ); ?>
+        <?php //get_template_part( 'template-parts/header/site-header' ); ?>
 
-	<div id="content" class="site-content">
-		<div id="primary" class="content-area">
-			<main id="main" class="site-main" role="main">
+        <div id="content" class="site-content">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main" role="main">
+
+                    <div class="text-center mt-3">
+                        <a href="<?php echo get_site_url();?>" class="logo-a">
+                            <img src="<?php echo get_template_directory_uri();?>/assets/images/logo.jpg" alt="">
+
+                        </a>
+
+                    </div>
+                    <div class="container">
+                        <ul>
+
+                            <ul class=" top-menu-ul">
+
+                                <?php
+                                    $main_menu = wp_get_menu_array('main menu');
+foreach ($main_menu as $menu_item) {
+
+$url = $menu_item['url'];
+$title = $menu_item['title'];
+$class = $menu_item['class'];
+
+$temp_arr=explode(get_site_url(),$url);
+$slug=str_replace('/en/','',$temp_arr[1]);
+$slug=str_replace('/cn/','',$slug);
+$slug=str_replace('/','',$slug);
+
+
+if(count($menu_item['children']))
+{
+  
+    echo '<li><a class="level-1 parent '.$class.'" href="'.$url.'">'.$title;
+    ?>
+                                <img class="arrow"
+                                    src="<?php echo get_template_directory_uri();?>/assets/images/white-arrow-enter.png"
+                                    alt="">
+
+                                <?php
+    echo'</a>';
+
+ 
+    echo '<ul class="mobile-menu-submenu">';
+?>
+
+                                <?php
+    
+    foreach ($menu_item['children'] as $sub_menu_item) 
+    {
+        $sub_url = $sub_menu_item['url'];
+        $sub_title = $sub_menu_item['title'];
+        
+        $sub_temp_arr=explode(get_site_url(),$sub_url);
+        $sub_slug=str_replace('/en/','',$sub_temp_arr[1]);
+        $sub_slug=str_replace('/cn/','',$sub_slug);
+        $sub_slug=str_replace('/','',$sub_slug);
+        echo'<li><a class="'.$sub_slug.'" href="'.$sub_url.'">'.$sub_title.'</a></li>';
+    }
+    echo '</ul>';
+
+}
+else
+{
+echo '<li><a class="level-1 '.$slug.' '.$class.'" href="'.$url.'">'.$title.'</a>';
+
+}
+echo'</li>';
+
+
+}
+
+
+// $langs= icl_get_languages('skip_missing=0&orderby=custom&order=asc&link_empty_to=');
+
+
+
+?>
+
+
+
+                            </ul>
+                    </div>
+
+                    <!-- <a href="#" class="mobile-menu-btn float-end"> -->
+
+                    <a id="nav-icon3" href="#" class="mobile-menu-btn float-end">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </a>
+                    <!-- </a> -->
+
+
+
+
+
+                    </ul>
+                    </ul>
+            </div>
--- wp-includes/theme-compat/header.php
+++ wp-includes/theme-compat/header.php
@@ -1,137 +1,76 @@
 <?php
 /**
- * The header.
- *
- * This is the template that displays all of the <head> section and everything up until main.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
  * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
+ * @subpackage Theme_Compat
+ * @deprecated 3.0.0
+ *
+ * This file is here for backward compatibility with old themes and will be removed in a future version.
  */
-
+_deprecated_file(
+	/* translators: %s: Template name. */
+	sprintf( __( 'Theme without %s' ), basename( __FILE__ ) ),
+	'3.0.0',
+	null,
+	/* translators: %s: Template name. */
+	sprintf( __( 'Please include a %s template in your theme.' ), basename( __FILE__ ) )
+);
 ?>
-<!doctype html>
-<html <?php language_attributes(); ?> <?php twentytwentyone_the_html_classes(); ?>>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="profile" href="https://gmpg.org/xfn/11" />
+    <meta http-equiv="Content-Type"
+        content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
+
+    <title><?php echo wp_get_document_title(); ?></title>
+
+    <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" type="text/css" media="screen" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+    <?php if ( file_exists( get_stylesheet_directory() . '/images/kubrickbgwide.jpg' ) ) { ?>
+    <style type="text/css" media="screen">
+    <?php // Checks to see whether it needs a sidebar.
+
+    if (empty($withcomments) && ! is_single()) {
+        ?>#page {
+            background: url("<?php bloginfo( 'stylesheet_directory' ); ?>/images/kubrickbg-<?php bloginfo( 'text_direction' ); ?>.jpg") repeat-y top;
+            border: none;
+        }
+
+        <?php
+    }
+
+    else {
+
+        // No sidebar. ?>
+        #page {
+            background: url("<?php bloginfo( 'stylesheet_directory' ); ?>/images/kubrickbgwide.jpg") repeat-y top;
+            border: none;
+        }
+
+        <?php
+    }
+
+    ?>
+    </style>
+    <?php } ?>
+
+    <?php
+if ( is_singular() ) {
+	wp_enqueue_script( 'comment-reply' );}
+?>
+
     <?php wp_head(); ?>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
 </head>
 
 <body <?php body_class(); ?>>
-    <?php wp_body_open(); ?>
-    <div id="page" class="site">
-        <a class="skip-link screen-reader-text" href="#content">
-            <?php esc_html_e( 'Skip to content', 'tutorcloud' ); ?></a>
+    <div id="page">
 
-        <?php //get_template_part( 'template-parts/header/site-header' ); ?>
-
-        <div id="content" class="site-content">
-            <div id="primary" class="content-area">
-                <main id="main" class="site-main" role="main">
-
-                    <div class="text-center mt-3">
-                        <a href="<?php echo get_site_url();?>" class="logo-a">
-                            <img src="<?php echo get_template_directory_uri();?>/assets/images/logo.jpg" alt="">
-
-                        </a>
-
-                    </div>
-                    <div class="container">
-                        <ul>
-
-                            <ul class=" top-menu-ul">
-
-                                <?php
-                                    $main_menu = wp_get_menu_array('main menu');
-foreach ($main_menu as $menu_item) {
-
-$url = $menu_item['url'];
-$title = $menu_item['title'];
-$class = $menu_item['class'];
-
-$temp_arr=explode(get_site_url(),$url);
-$slug=str_replace('/en/','',$temp_arr[1]);
-$slug=str_replace('/cn/','',$slug);
-$slug=str_replace('/','',$slug);
-
-
-if(count($menu_item['children']))
-{
-  
-    echo '<li><a class="level-1 parent '.$class.'" href="'.$url.'">'.$title;
-    ?>
-                                <img class="arrow"
-                                    src="<?php echo get_template_directory_uri();?>/assets/images/white-arrow-enter.png"
-                                    alt="">
-
-                                <?php
-    echo'</a>';
-
- 
-    echo '<ul class="mobile-menu-submenu">';
-?>
-
-                                <?php
-    
-    foreach ($menu_item['children'] as $sub_menu_item) 
-    {
-        $sub_url = $sub_menu_item['url'];
-        $sub_title = $sub_menu_item['title'];
-        
-        $sub_temp_arr=explode(get_site_url(),$sub_url);
-        $sub_slug=str_replace('/en/','',$sub_temp_arr[1]);
-        $sub_slug=str_replace('/cn/','',$sub_slug);
-        $sub_slug=str_replace('/','',$sub_slug);
-        echo'<li><a class="'.$sub_slug.'" href="'.$sub_url.'">'.$sub_title.'</a></li>';
-    }
-    echo '</ul>';
-
-}
-else
-{
-echo '<li><a class="level-1 '.$slug.' '.$class.'" href="'.$url.'">'.$title.'</a>';
-
-}
-echo'</li>';
-
-
-}
-
-
-// $langs= icl_get_languages('skip_missing=0&orderby=custom&order=asc&link_empty_to=');
-
-
-
-?>
-
-
-
-                            </ul>
-                    </div>
-
-                    <!-- <a href="#" class="mobile-menu-btn float-end"> -->
-
-                    <a id="nav-icon3" href="#" class="mobile-menu-btn float-end">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </a>
-                    <!-- </a> -->
-
-
-
-
-
-                    </ul>
-                    </ul>
+        <div id="header" role="banner">
+            <div id="headerimg">
+                <h1><a href="<?php echo home_url(); ?>/"><?php bloginfo( 'name' ); ?></a></h1>
+                <div class="description"><?php bloginfo( 'description' ); ?></div>
             </div>
+        </div>
+        <hr />
