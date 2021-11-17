605d6af06f4c165ae089c8e17ca229e06153a692
kaiyu li <krisfk@gmail.com>
2021-11-17 19:54:29 +0800

    .

--- wp-content/themes/tutorcloud/page-home.php
+++ wp-content/themes/tutorcloud/page-home.php
@@ -35,33 +35,33 @@ get_header();
     </div>
 
     <div class="row gx-5 ">
-        <div class="col-4 mb-4"> <img class="w-100"
+        <div class="col-4 mb-4"> <img class="w-100 pt-icon"
                 src="<?php echo get_template_directory_uri();?>/assets/images/icon-1.png" alt="">
             <div class="pt-title mt-3">優良師資</div>
             <div class="pt-content mt-3">所有導師均為現職大學生及具有學士學位或以上資歷，確保導師知識水平及能力，為學生提供最優質的教育。</div>
         </div>
-        <div class="col-4 mb-4"><img class="w-100"
+        <div class="col-4 mb-4"><img class="w-100 pt-icon"
                 src="<?php echo get_template_directory_uri();?>/assets/images/icon-2.png" alt="">
             <div class="pt-title mt-3">簡易登記</div>
             <div class="pt-content mt-3">登記過程簡易快速，只需填寫個案基本資料，最快2分鐘內完成登記。</div>
         </div>
-        <div class="col-4 mb-4"><img class="w-100"
+        <div class="col-4 mb-4"><img class="w-100 pt-icon"
                 src="<?php echo get_template_directory_uri();?>/assets/images/icon-3.png" alt="">
             <div class="pt-title mt-3">彈性課堂</div>
             <div class="pt-content mt-3">學生可以自由選擇上課地點及時間，不論是上門、會所、視像補習都由學生自行決定。不像傳統補習社限制時間和地點，私人補習配對解決空間和時間的限制。</div>
         </div>
 
-        <div class="col-4 mb-4"> <img class="w-100"
+        <div class="col-4 mb-4"> <img class="w-100 pt-icon"
                 src="<?php echo get_template_directory_uri();?>/assets/images/icon-4.png" alt="">
             <div class="pt-title mt-3">即時配對</div>
             <div class="pt-content mt-3">所有導師均為現職大學生及具有學士學位或以上資歷，確保導師知識水平及能力，為學生提供最優質的教育。</div>
         </div>
-        <div class="col-4 mb-4"><img class="w-100"
+        <div class="col-4 mb-4"><img class="w-100 pt-icon"
                 src="<?php echo get_template_directory_uri();?>/assets/images/icon-5.png" alt="">
             <div class="pt-title mt-3">收費透明</div>
             <div class="pt-content mt-3">登記過程簡易快速，只需填寫個案基本資料，最快2分鐘內完成登記。</div>
         </div>
-        <div class="col-4 mb-4"><img class="w-100"
+        <div class="col-4 mb-4"><img class="w-100 pt-icon"
                 src="<?php echo get_template_directory_uri();?>/assets/images/icon-6.png" alt="">
             <div class="pt-title mt-3">學生主導</div>
             <div class="pt-content mt-3">學生可以自由選擇上課地點及時間，不論是上門、會所、視像補習都由學生自行決定。不像傳統補習社限制時間和地點，私人補習配對解決空間和時間的限制。</div>
