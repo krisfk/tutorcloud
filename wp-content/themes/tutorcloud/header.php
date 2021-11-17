a146e0f962e38cacae8281f5d8923d34b8a25205
kaiyu li <krisfk@gmail.com>
2021-11-17 16:17:53 +0800

    .

--- wp-content/themes/tutorcloud/header.php
+++ wp-content/themes/tutorcloud/header.php
@@ -47,11 +47,11 @@
 
                     </div>
                     <div class="container">
-                        <ul>
+                        <!-- <ul> -->
 
-                            <ul class=" top-menu-ul">
+                        <ul class=" top-menu-ul">
 
-                                <?php
+                            <?php
                                     $main_menu = wp_get_menu_array('main menu');
 foreach ($main_menu as $menu_item) {
 
@@ -70,18 +70,18 @@ if(count($menu_item['children']))
   
     echo '<li><a class="level-1 parent '.$class.'" href="'.$url.'">'.$title;
     ?>
-                                <img class="arrow"
-                                    src="<?php echo get_template_directory_uri();?>/assets/images/white-arrow-enter.png"
-                                    alt="">
+                            <img class="arrow"
+                                src="<?php echo get_template_directory_uri();?>/assets/images/white-arrow-enter.png"
+                                alt="">
 
-                                <?php
+                            <?php
     echo'</a>';
 
  
     echo '<ul class="mobile-menu-submenu">';
 ?>
 
-                                <?php
+                            <?php
     
     foreach ($menu_item['children'] as $sub_menu_item) 
     {
@@ -116,7 +116,7 @@ echo'</li>';
 
 
 
-                            </ul>
+                        </ul>
                     </div>
 
                     <!-- <a href="#" class="mobile-menu-btn float-end"> -->
