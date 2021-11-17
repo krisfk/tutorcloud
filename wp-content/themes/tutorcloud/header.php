c39ecd85f047704132ab6b6e6a1c381ce9d556ba
kaiyu li <krisfk@gmail.com>
2021-11-17 16:28:55 +0800

    .

--- wp-content/themes/tutorcloud/header.php
+++ wp-content/themes/tutorcloud/header.php
@@ -42,9 +42,9 @@
 
                     <div class="top-div">
 
-                        <div class="container">
+                        <div class="container h-100">
 
-                            <div class="row">
+                            <div class="row h-100">
                                 <div class="col-4">
                                     <img src="<?php echo get_template_directory_uri();?>/assets/images/logo.png" alt="">
                                 </div>
