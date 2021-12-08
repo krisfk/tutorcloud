434eb1ba64a2acb2396cd33f5da1a010906da9b9
kaiyu li <krisfk@gmail.com>
2021-12-09 06:05:45 +0800

    .

--- wp-content/themes/tutorcloud/page-tutor-register.php
+++ wp-content/themes/tutorcloud/page-tutor-register.php
@@ -32,7 +32,7 @@ get_header();
             <img class="tutor-reg-icon"
                 src="<?php echo get_template_directory_uri();?>/assets/images/tutor-reg-icon.png" alt="">
         </div>
-        <div class="col-9">年滿十七歲或以上，正就讀大學及具大學學士學位或以上程度學歷之自然人且擁有簽訂合約的心智和法律能
+        <div class="col-5">年滿十七歲或以上，正就讀大學及具大學學士學位或以上程度學歷之自然人且擁有簽訂合約的心智和法律能
             力之人士，以及獲其憲章、附例及公司組織章程大綱批准其與TutorCloud 師魂簽訂此合約的公司，方可申請成
             為導師。TutorCloud 師魂保留拒絕任何人士申請成為導師或終止任何人士之導師資格而不作任何解釋之權利。
             不符合上述資格者不得使用服務。</div>
