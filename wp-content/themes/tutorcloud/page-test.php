65fd817b8cdbda6f430113150342a9f8441d1727
kaiyu li <krisfk@gmail.com>
2021-12-08 17:30:35 +0800

    .

--- wp-content/themes/tutorcloud/page-test.php
+++ wp-content/themes/tutorcloud/page-test.php
@@ -7,7 +7,7 @@ if($_POST)
 }
 ?>
 
-<form action="" method="post" enctype="multipart/form-data">
+<form action="" method="post">
 
     <input type="file" name="upload_file" id="upload_file">
     <input type="submit">
