b8578ca24f71c04fbcdf09a9d351162ca5790de7
kaiyu li <krisfk@gmail.com>
2021-12-09 06:54:02 +0800

    .

--- wp-content/themes/tutorcloud/page-apply-case.php
+++ wp-content/themes/tutorcloud/page-apply-case.php
@@ -58,16 +58,19 @@ get_header();
 
             <ul class="tutor-list-ul">
                 <li class="">
-                    <div class="row">
-                        <div class='col-2'>T210001 <br>女 <img
-                                src="<?php echo get_template_directory_uri();?>/assets/images/f-icon.png" alt=""></div>
-                        <div class='col-2'>香港中文大學 <br>護理系 </div>
-                        <div class='col-2'>大學生</div>
-                        <div class='col-2'>可接受最低時薪： <br>$160</div>
-                        <div class='col-2'>可教授科目： <br>數學 化學 經濟</div>
-                        <div class='col-2'>fdas</div>
+                    <a href="javascript:void(0);">
+                        <div class="row">
+                            <div class='col-2'>T210001 <br>女 <img
+                                    src="<?php echo get_template_directory_uri();?>/assets/images/f-icon.png" alt="">
+                            </div>
+                            <div class='col-2'>香港中文大學 <br>護理系 </div>
+                            <div class='col-2'>大學生</div>
+                            <div class='col-2'>可接受最低時薪： <br>$160</div>
+                            <div class='col-2'>可教授科目： <br>數學 化學 經濟</div>
+                            <div class='col-2'>fdas</div>
 
-                    </div>
+                        </div>
+                    </a>
                 </li>
             </ul>
         </div>
