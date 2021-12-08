cbe61b61aca96e2eaa1df3e6cc9df4fb28dd259d
kaiyu li <krisfk@gmail.com>
2021-12-08 17:31:02 +0800

    .

--- wp-content/themes/tutorcloud/page-test.php
+++ wp-content/themes/tutorcloud/page-test.php
@@ -2,12 +2,12 @@
 
 if($_POST)
 {
-	 echo 2;
+	 echo 3;
 	print_r($_FILES);
 }
 ?>
 
-<form action="" method="post">
+<form action="" method="post" enctype="multipart/form-data">
 
     <input type="file" name="upload_file" id="upload_file">
     <input type="submit">
