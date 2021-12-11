993bffd88ce00874f567aba983d97c8bcfe099ea
kaiyu li <krisfk@gmail.com>
2021-12-12 00:15:40 +0800

    .

--- wp-content/themes/tutorcloud/page-apply-case.php
+++ wp-content/themes/tutorcloud/page-apply-case.php
@@ -1162,31 +1162,31 @@ Year 6 -->
                                     <td>
 
                                         <div class="checkbox-div"><input type="checkbox" class="form-check-input"
-                                                id="day_value_1" name="day[]" value="1" /><label
+                                                id="day_value_1" name="day[]" value="星期一" /><label
                                                 for="day_value_1">星期一</label>
                                         </div>
                                         <div class="checkbox-div"><input type="checkbox" class="form-check-input"
-                                                id="day_value_2" name="day[]" value="2" /><label
+                                                id="day_value_2" name="day[]" value="星期二" /><label
                                                 for="day_value_2">星期二</label>
                                         </div>
                                         <div class="checkbox-div"><input type="checkbox" class="form-check-input"
-                                                id="day_value_3" name="day[]" value="3" /><label
+                                                id="day_value_3" name="day[]" value="星期三" /><label
                                                 for="day_value_3">星期三</label>
                                         </div>
                                         <div class="checkbox-div"><input type="checkbox" class="form-check-input"
-                                                id="day_value_4" name="day[]" value="4" /><label
+                                                id="day_value_4" name="day[]" value="星期四" /><label
                                                 for="day_value_4">星期四</label>
                                         </div>
                                         <div class="checkbox-div"><input type="checkbox" class="form-check-input"
-                                                id="day_value_5" name="day[]" value="5" /><label
+                                                id="day_value_5" name="day[]" value="星期五" /><label
                                                 for="day_value_5">星期五</label>
                                         </div>
                                         <div class="checkbox-div"><input type="checkbox" class="form-check-input"
-                                                id="day_value_6" name="day[]" value="6" /><label
+                                                id="day_value_6" name="day[]" value="星期六" /><label
                                                 for="day_value_6">星期六</label>
                                         </div>
                                         <div class="checkbox-div"><input type="checkbox" class="form-check-input"
-                                                id="day_value_7" name="day[]" value="7" /><label
+                                                id="day_value_7" name="day[]" value="星期日" /><label
                                                 for="day_value_7">星期日</label>
                                         </div>
 
