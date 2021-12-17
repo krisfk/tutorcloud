f12586a4edc10efe240aefc581d46283ff560578
kaiyu li <krisfk@gmail.com>
2021-12-18 04:14:59 +0800

    .

--- wp-content/themes/tutorcloud/page-test.php
+++ wp-content/themes/tutorcloud/page-test.php
@@ -1,24 +1,29 @@
 <?php 
 
-if($_POST)
-{
-    echo 'post';
-}
+// if($_POST)
+// {
+//     echo 'post';
+// }
+
+        // $tutor_id = 'T'.
+        $post_id= 999;
+        $tutor_id = str_pad($post_id, 9, '0', STR_PAD_LEFT);
+    echo $tutor_id;
 
 ?>
 <?php
 
 
-if($_FILES["upload_file"]['size'])
-{
-	//  echo 'have file';
-	print_r($_FILES);
-}
+// if($_FILES["upload_file"]['size'])
+// {
+// 	//  echo 'have file';
+// 	print_r($_FILES);
+// }
 ?>
 
-<form action="" method="post" enctype="multipart/form-data">
+<!-- <form action="" method="post" enctype="multipart/form-data">
 
     <input type="text" name="test">
     <input type="file" name="upload_file" id="upload_file">
     <input type="submit">
-</form>
+</form> -->
