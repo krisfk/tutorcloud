c6196274aff3850a8d3230ec398b22567e3d87ad
kaiyu li <krisfk@gmail.com>
2021-12-18 05:59:31 +0800

    .

--- wp-content/themes/tutorcloud/page-apply-case.php
+++ wp-content/themes/tutorcloud/page-apply-case.php
@@ -20,7 +20,7 @@ get_header();
         <div>香港中文大學及聖公會曾肇添中學畢業。全職補習。5年以上數學補習經驗。公開試HKDSE化學科奪得5**最高成績，提供超過20年公開試題目特訓，公開試HKDSE數學科5*級成績，小一至中六數學科及化學科成績保證。
         </div>
 
-        <div class="row">
+        <div class="row gx-0">
             <div class="col-6">
                 <table class="detail-content-table">
 
