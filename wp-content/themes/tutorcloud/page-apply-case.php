70822d8691c4f943b83e72fba65aead80e714050
kaiyu li <krisfk@gmail.com>
2021-12-19 03:51:47 +0800

    .

--- wp-content/themes/tutorcloud/page-apply-case.php
+++ wp-content/themes/tutorcloud/page-apply-case.php
@@ -1616,10 +1616,6 @@ $(function() {
 
 
 
-    $('.list-close-btn').click(function() {
-        var select_tutor_id = $(this).prev('span').html();
-        alert(select_tutor_id);
-    });
     $('.add-btn-a').click(function() {
 
         var select_tutor_id = $(this).closest('li').attr('data-tutor-id');
@@ -1632,6 +1628,10 @@ $(function() {
 
             $('.added-list ul').append(added_to_cart_html);
 
+            $('.list-close-btn').click(function() {
+                var select_tutor_id = $(this).prev('span').html();
+                // alert(select_tutor_id);
+            });
 
 
         } else {
