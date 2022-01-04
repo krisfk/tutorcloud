094981c5386a98398439796bb5a37e7b42671249
kaiyu li <krisfk@gmail.com>
2022-01-04 22:58:44 +0800

    .

--- wp-content/themes/tutorcloud/page-apply-case.php
+++ wp-content/themes/tutorcloud/page-apply-case.php
@@ -444,7 +444,7 @@ $loop = new WP_Query( $args );
     
 while ( $loop->have_posts() ) { 
     $loop->the_post(); 
-    echo 1;
+    // echo 1;
     // implode(', ', $Array)
     ?>
                     <li class="tutor-content-li" data-class-area="<?php echo implode(',', get_field('class_area'));?>"
