a5c2f48fd55a584d35d70b4fb2a00eeeff8e9c25
kaiyu li <krisfk@gmail.com>
2022-01-04 23:15:15 +0800

    .

--- wp-content/themes/tutorcloud/page-apply-case.php
+++ wp-content/themes/tutorcloud/page-apply-case.php
@@ -438,7 +438,7 @@ get_header();
         ),
         array(
             'key'     => 'tutor_level',
-            'value'   =>  '大學生',
+            'value'   =>  '碩士生',
             'compare' => 'LIKE',
         )
     )
