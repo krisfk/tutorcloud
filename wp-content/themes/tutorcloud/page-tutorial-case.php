91a8d0c914c028a64f03751537faec47cb734aa4
kaiyu li <krisfk@gmail.com>
2022-02-10 23:23:49 +0800

    .

--- wp-content/themes/tutorcloud/page-tutorial-case.php
+++ wp-content/themes/tutorcloud/page-tutorial-case.php
@@ -1229,7 +1229,8 @@ while ( $loop->have_posts() ) {
 
                         <div class='col-4 data-column'>
                             每週堂數 / 時數: <br>
-                            <?php echo get_field('lesson_per_week');?>;<?php echo get_field('hour_per_lesson');?>
+                            一星期<?php echo get_field('lesson_per_week');?>堂;一堂<?php echo get_field('hour_per_lesson');?>小時
+
 
                         </div>
 
