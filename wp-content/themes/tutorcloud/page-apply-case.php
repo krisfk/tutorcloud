14830a2967071cbc85ff6bb740b4ab0e5076ce99
kaiyu li <krisfk@gmail.com>
2022-02-12 04:35:40 +0800

    .

--- wp-content/themes/tutorcloud/page-apply-case.php
+++ wp-content/themes/tutorcloud/page-apply-case.php
@@ -1525,6 +1525,29 @@ while ( $loop->have_posts() ) {
                 ?>
                 </ul>
 
+                <div class="pagination mt-5">
+
+                    <?php 
+               
+        echo paginate_links( array(
+            'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
+            'total'        => $loop->max_num_pages,
+            'current'      => max( 1, get_query_var( 'paged' ) ),
+            'format'       => '?paged=%#%',
+            'show_all'     => false,
+            'type'         => 'plain',
+            'end_size'     => 2,
+            'mid_size'     => 1,
+            'prev_next'    => true,
+            'prev_text'    => sprintf( '<i></i> %1$s', __( '&lt;', 'text-domain' ) ),
+            'next_text'    => sprintf( '%1$s <i></i>', __( '&gt;', 'text-domain' ) ),
+            'add_args'     => false,
+            'add_fragment' => '',
+        ) );
+    ?>
+                </div>
+
+
 
                 <div class="student-form-div">
                     <h3 class="mt-1 mb-3 p-0">登記學生資料</h2>
