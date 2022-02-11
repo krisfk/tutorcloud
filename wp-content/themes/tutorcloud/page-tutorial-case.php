d1c662010ab3af7a49260255fbdff9494edbb530
kaiyu li <krisfk@gmail.com>
2022-02-12 03:52:33 +0800

    .

--- wp-content/themes/tutorcloud/page-tutorial-case.php
+++ wp-content/themes/tutorcloud/page-tutorial-case.php
@@ -451,16 +451,23 @@ while ( $loop->have_posts() ) {
             </ul>
 
 
-
             <div class="pagination">
-
-                <?php
-           the_posts_pagination( array(
-            'mid_size' => 2,
-            'prev_text' => __( 'Previous Page', 'textdomain' ),
-            'next_text' => __( 'Next Page', 'textdomain' ),
-            ) );
-            
+                <?php 
+        echo paginate_links( array(
+            'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
+            'total'        => $query->max_num_pages,
+            'current'      => max( 1, get_query_var( 'paged' ) ),
+            'format'       => '?paged=%#%',
+            'show_all'     => false,
+            'type'         => 'plain',
+            'end_size'     => 2,
+            'mid_size'     => 1,
+            'prev_next'    => true,
+            'prev_text'    => sprintf( '<i></i> %1$s', __( 'Newer Posts', 'text-domain' ) ),
+            'next_text'    => sprintf( '%1$s <i></i>', __( 'Older Posts', 'text-domain' ) ),
+            'add_args'     => false,
+            'add_fragment' => '',
+        ) );
     ?>
             </div>
 
