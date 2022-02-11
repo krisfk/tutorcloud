8389a73f1a2a0a69343c5d6f32f5ee1e9b203ccc
kaiyu li <krisfk@gmail.com>
2022-02-11 17:47:21 +0800

    .

--- wp-content/themes/tutorcloud/page-tutorial-case.php
+++ wp-content/themes/tutorcloud/page-tutorial-case.php
@@ -1122,6 +1122,7 @@ get_header();
             // echo 99;
             if($_POST && $_POST['filter-form'])
             {
+                echo $_POST['class-area'];
                 if($_POST['gender'])
                 {
                     array_push($meta_query_arr,array('key' => 'gender','value' =>$_POST['gender'] ,'compare' => 'LIKE'));
