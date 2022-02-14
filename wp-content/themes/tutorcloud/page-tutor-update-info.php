d626fb2aeeaa27457d3dc33f2713e1e9f4a7dc22
kaiyu li <krisfk@gmail.com>
2022-02-14 22:31:11 +0800

    .

--- wp-content/themes/tutorcloud/page-tutor-update-info.php
+++ wp-content/themes/tutorcloud/page-tutor-update-info.php
@@ -25,6 +25,7 @@ if($_POST['form-type']=='tutor-info-form-1')
     $occupation=$_POST['occupation'];
     $living_area=$_POST['living_area'];
     $post_id = $_SESSION['tutor_post_id'];
+    echo $post_id;
     add_post_meta($post_id, 'chi_name', $chi_name);
     add_post_meta($post_id, 'eng_name', $eng_name);
     add_post_meta($post_id, 'nick_name', $nick_name);
