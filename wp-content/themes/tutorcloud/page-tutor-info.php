2763a49f33657c0550d14f66f95743161631b4c4
kaiyu li <krisfk@gmail.com>
2022-02-15 20:49:26 +0800

    .

--- wp-content/themes/tutorcloud/page-tutor-info.php
+++ wp-content/themes/tutorcloud/page-tutor-info.php
@@ -497,16 +497,16 @@ $the_query->the_post();
                             </td>
                             <td>
 
+                                <?php
+                                $file_src = wp_get_attachment_url(get_field('proof1'));
+                                $pieces = explode("/", $file_src);                   
+                                ?>
                                 <a class="file-a" href="<?php echo $file_src;?>">
-
+                                    <?php echo $pieces[count($pieces)-1];?>
                                     <img class="file-icon"
                                         src="<?php echo get_template_directory_uri().'/assets/images/file-icon.png';?>"
                                         alt="">
-                                    <?php
-                                                           $file_src = wp_get_attachment_url(get_field('proof1'));
-                                                           $pieces = explode("/", $file_src);
-                                                              echo $pieces[count($pieces)-1];
-                                ?></a>
+                                </a>
 
                                 <a href="javascript:void(0);" class="change-file-btn">更改檔案</a>
 
