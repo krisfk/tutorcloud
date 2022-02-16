e179ca92e4af711f271f61a9de9e45302381074c
kaiyu li <krisfk@gmail.com>
2022-02-17 07:18:59 +0800

    .

--- wp-content/themes/tutorcloud/page-tutor-excel.php
+++ wp-content/themes/tutorcloud/page-tutor-excel.php
@@ -6,10 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 </head>
 
 <body>
 
+    <a href="javascript:void(0);" class="getfile">EXPORT</a>
 
 
     <?php
@@ -116,7 +119,56 @@ $table='<table class="excel-table" id="excel-table">
     }
     </style>
 
+    <script type="text/javascript">
+    $(function() {
+        $('.getfile').click(
+            function() {
+                exportTableToCSV.apply(this, [$('#thetable'), 'filename.csv']);
+            });
 
+    })
+
+    function exportTableToCSV($table, filename) {
+
+        var $rows = $table.find('tr:has(td)'),
+
+            // Temporary delimiter characters unlikely to be typed by keyboard
+            // This is to avoid accidentally splitting the actual contents
+            tmpColDelim = String.fromCharCode(11), // vertical tab character
+            tmpRowDelim = String.fromCharCode(0), // null character
+
+            // actual delimiter characters for CSV format
+            colDelim = '","',
+            rowDelim = '"\r\n"',
+
+            // Grab text from table into CSV formatted string
+            csv = '"' + $rows.map(function(i, row) {
+                var $row = $(row),
+                    $cols = $row.find('td');
+
+                return $cols.map(function(j, col) {
+                    var $col = $(col),
+                        text = $col.text();
+
+                    return text.replace('"', '""'); // escape double quotes
+
+                }).get().join(tmpColDelim);
+
+            }).get().join(tmpRowDelim)
+            .split(tmpRowDelim).join(rowDelim)
+            .split(tmpColDelim).join(colDelim) + '"',
+
+            // Data URI
+            csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(csv);
+
+        $(this)
+            .attr({
+                'download': filename,
+                'href': csvData,
+                'target': '_blank'
+            });
+    }
+    </script>
 
 </body>
 
