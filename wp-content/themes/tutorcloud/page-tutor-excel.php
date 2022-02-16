7bfe3213bcdc0168f8722ab58cdc3fdf0bf5dae9
kaiyu li <krisfk@gmail.com>
2022-02-17 07:15:10 +0800

    .

--- wp-content/themes/tutorcloud/page-tutor-excel.php
+++ wp-content/themes/tutorcloud/page-tutor-excel.php
@@ -95,36 +95,7 @@ $table='<table class="excel-table" id="excel-table">
 
 
 
-    <?php
-    $html = str_get_html($table);
 
-
-
-    // header('Content-type: application/ms-excel');
-    // header('Content-Disposition: attachment; filename=sample.csv');
-    
-    // $fp = fopen("php://output", "w");
-    
-    // foreach($html->find('tr') as $element)
-    // {
-    //         $th = array();
-    //         foreach( $element->find('th') as $row)  
-    //         {
-    //             $th [] = $row->plaintext;
-    //         }
-    
-    //         $td = array();
-    //         foreach( $element->find('td') as $row)  
-    //         {
-    //             $td [] = $row->plaintext;
-    //         }
-    //         !empty($th) ? fputcsv($fp, $th) : fputcsv($fp, $td);
-    // }
-    
-    
-    // fclose($fp);
-    
-    ?>
     <style type="text/css">
     table {
         width: max-content;
