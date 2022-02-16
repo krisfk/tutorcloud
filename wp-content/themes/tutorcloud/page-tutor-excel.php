da5e7fbbb9eb50a3d9ea542830757d834b0f0051
kaiyu li <krisfk@gmail.com>
2022-02-17 06:10:04 +0800

    .

--- wp-content/themes/tutorcloud/page-tutor-excel.php
+++ wp-content/themes/tutorcloud/page-tutor-excel.php
@@ -1,1 +1,36 @@
-fdaf
+<?php
+
+// Get arguments for all posts
+$args = array( 
+	'post_type' => 'tutor',
+    'post_status' => 'publish',
+    'posts_per_page' => -1,
+);
+
+// Create the array for all posts
+$all_posts = array();
+
+// Set up the query
+$the_query = new WP_Query( $args );
+
+// Setup the loop
+if ( $the_query->have_posts() ):
+
+	while ( $the_query->have_posts() ): $the_query->the_post();
+
+		// Get all fields
+		$fields = get_fields();
+
+		// Push each $fields array into the $all_posts array
+		array_push($all_posts, $fields);
+
+	endwhile;
+
+	// Restore original Post Data
+	wp_reset_postdata();
+
+	// Print the result here and do what you choose
+	print_r($all_posts);
+
+endif;
+?>
