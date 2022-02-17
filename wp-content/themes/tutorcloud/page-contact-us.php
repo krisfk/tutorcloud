76f226baea37167bd20a6e747b5613d1a9b33a59
kaiyu li <krisfk@gmail.com>
2022-02-17 17:51:30 +0800

    .

---
+++ wp-content/themes/tutorcloud/page-contact-us.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+?>
+
+<div class="breadcrumb mt-4">
+
+    主頁 > 聯絡我們
+</div>
+<div class="container mt-5">
+
+    <div class="text-center">
+        <h2>聯絡我們
+            <div class="bar"></div>
+        </h2>
+    </div>
+
+
+
+
+
+
+
+
+
+</div>
+<?php
+get_footer();
