b3295cd056ed384e279c5e27113d98b5b7d92cec
kaiyu li <krisfk@gmail.com>
2022-02-17 18:16:08 +0800

    .

---
+++ wp-content/themes/tutorcloud/page-forgot-pw.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+?>
+
+<div class="breadcrumb mt-4">
+
+    主頁 > 忘記密碼
+</div>
+<div class="container mt-5">
+
+    <div class="text-center">
+        <h2>忘記密碼
+            <div class="bar"></div>
+        </h2>
+    </div>
+
+
+
+
+
+
+
+
+
+</div>
+<?php
+get_footer();
