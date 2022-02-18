42be37812bcacd290121d123ec14f248d5e3700e
kaiyu li <krisfk@gmail.com>
2022-02-19 00:41:22 +0800

    .

--- wp-content/themes/tutorcloud/single-student.php
+++ wp-content/themes/tutorcloud/single-student.php
@@ -458,6 +458,7 @@ if (!$is_admin) {
     // implode(', ', $Array)
     ?>
                 999
+                <?php echo get_field('student_id');?>
                 <li class="student-content-li" data-student-id="<?php echo get_field('student_id');?>"
                     data-student-gender="<?php echo get_field('gender');?>"
                     data-salary="<?php echo get_field('cost_per_ppl');?>"
