e11532b2cdfb05db2aa117a90ae8e83d9e674e48
kaiyu li <krisfk@gmail.com>
2022-02-19 22:16:41 +0800

    .

--- wp-content/themes/tutorcloud/page-tutorial-case.php
+++ wp-content/themes/tutorcloud/page-tutorial-case.php
@@ -452,7 +452,7 @@ get_header();
 
                 if($_POST['subjects'])
                 {
-                    // print_r($_POST['subjects']);
+                    print_r($_POST['subjects']);
                     array_push($meta_query_arr,array('key'=>'subjects','value'=>$_POST['subjects'],'compare' => 'LIKE'));
                 }
 
