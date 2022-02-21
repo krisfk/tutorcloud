7653967fa68ed557e8ddba62e7997bf3af847aa3
kaiyu li <krisfk@gmail.com>
2022-02-22 00:29:38 +0800

    .

---
+++ wp-content/themes/tutorcloud/page-test copy.php
@@ -0,0 +1,33 @@
+<?php 
+
+// $fields = get_fields(446);
+// print_r($fields);
+// if($_POST)
+// {
+//     echo 'post';
+// }
+
+// echo 11;
+        // $tutor_id = 'T'.
+        // $post_id= 999;
+        // $tutor_id = str_pad($post_id, 5, '0', STR_PAD_LEFT);
+        // echo 'T'.substr(date('Y'),2,2).$tutor_id;
+    // echo $tutor_id;
+
+?>
+<?php
+
+
+// if($_FILES["upload_file"]['size'])
+// {
+// 	//  echo 'have file';
+// 	print_r($_FILES);
+// }
+?>
+
+<!-- <form action="" method="post" enctype="multipart/form-data">
+
+    <input type="text" name="test">
+    <input type="file" name="upload_file" id="upload_file">
+    <input type="submit">
+</form> -->
