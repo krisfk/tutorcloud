b59dc6f341789af8c9bc84012d3344d022848a75
kaiyu li <krisfk@gmail.com>
2022-02-22 00:01:30 +0800

    .

--- wp-content/themes/tutorcloud/page-tutor-excel.php
+++ wp-content/themes/tutorcloud/page-tutor-excel.php
@@ -51,15 +51,15 @@ if ( $the_query->have_posts() ):
 
 	while ( $the_query->have_posts() ): $the_query->the_post();
 		$fields = get_fields();
-        if(!$save_th)
-        {
+        // if(!$save_th)
+        // {
             // echo 1;
             foreach( $fields as $name => $value ){
                 echo 1;
                 $field = get_field_object($name); 
                 array_push($table_th_arr,$field['label']);
             }
-        }
+        // }
             $save_th =true;
 
 array_push($all_posts, $fields);
